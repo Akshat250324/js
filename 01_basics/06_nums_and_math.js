17b9ff3c6eefc9a0d8066e6ea01ffb11e80fa0e7
AKSHAT KEMBULKAR <kembulkarakshat9967@gmail.com>
2025-06-07 08:43:12 +0000

    math

---
+++ 01_basics/06_nums_and_math.js
@@ -0,0 +1,41 @@
+const score = 400
+// console.log(score)
+const balance = new Number(100)
+// console.log(balance);
+
+// console.log(balance.toString().length);
+// console.log(balance.toFixed(3));
+
+const otherNumber = 1123.45465
+
+// console.log(otherNumber.toPrecision(3));
+
+const hunderds = 100000 
+// console.log(hunderds.toLocaleString('en-IN'));
+
+// +++++++++++++++++++++++ Maths +++++++++++++++++++++++
+// console.log(Math);
+// console.log(Math.abs(-4));  // absolute converts - val to plus
+// console.log(Math.round(4.5));
+// console.log(Math.ceil(4.2));
+// console.log(Math.floor(4.9));
+// console.log(Math.min(4,3,5,6));
+// console.log(Math.max(3,4,6,2,7));
+
+console.log((Math.random()*10)+1);
+// trick to get proper values between some range
+const min = 10
+const max = 20
+
+console.log(Math.floor(Math.random() * (max-min+1))+min);
+
+
+
+
+
+
+
+
+
+
+
--- 01_basics/datatypes-summary.js
+++ 01_basics/datatypes-summary.js
@@ -49,3 +49,32 @@ console.log(typeof anotherId);
 // typeof Math returns "object" (this is due to a known issue in JavaScript).
 // typeof null returns "object" (another known issue in JavaScript).
 // typeof alert returns "function".
+
+
+// ++++++++++++++++++++++++++++++++++++++++++++++++++++
+// MEMORY -stack and heap
+// Stack(Used in primirive types) , Heap(non-primitive)
+
+let myYoutubename = "akshatkembulkar.com"
+
+let anotherName = myYoutubename
+anotherName = "AK"
+console.log(anotherName);
+
+console.log(myYoutubename);
+
+// Because here a copy is made and value is changed in the copy not in real one
+
+let user = {
+    email:"user@123",
+    upi: "user@ybl",
+}
+
+let userTwo = user
+
+userTwo.email = "akshat@google.com"
+
+console.log(user.email);
+console.log(userTwo.email);
+
+// In non-primitive datatypes the value is stored in heap instead of stack , and no copy file is created , the changes are made directly  in the original file

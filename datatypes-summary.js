0874b8e00494bab00f3fa7eab5e64661f211284d
AKSHAT KEMBULKAR <kembulkarakshat9967@gmail.com>
2025-06-07 06:43:29 +0000

    datatypes-summary

---
+++ datatypes-summary.js
@@ -0,0 +1,51 @@
+// # Primitive
+
+// 7 types : String, Number, Boolean, Null, undefined, Symbol, BigInt.
+
+const score = false
+// JS is dynamically typed
+// Reference (Non primitive)
+
+//Array, Objects, Functions
+
+const outsideTemp = null
+let userEmail;
+
+const id = Symbol('123')
+const anotherId = Symbol('123')
+
+console.log(id === anotherId);
+
+
+const bigNumber = 23438432868232n
+
+const heros = ["shaktiman","naagraaj","doga"]
+let myobj = {
+    name:"Akshat",
+    age:18,
+}
+
+const myfunction = function(){
+    console.log("Hello World!");
+    
+}
+
+console.log(typeof bigNumber);
+console.log(typeof outsideTemp );
+
+console.log(typeof score);
+console.log(typeof myfunction);
+console.log(typeof heros);
+
+console.log(typeof anotherId);
+
+
+// typeof undefined returns "undefined".
+// typeof 0 returns "number".
+// typeof 10n returns "bigint".
+// typeof true returns "boolean".
+// typeof "foo" returns "string".
+// typeof Symbol("id") returns "symbol".
+// typeof Math returns "object" (this is due to a known issue in JavaScript).
+// typeof null returns "object" (another known issue in JavaScript).
+// typeof alert returns "function".
